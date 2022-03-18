dad34749793e208cf603cd4b3b6e9feb4fbdd93f
rintosaputro <saputrorinto@gmail.com>
2022-03-18 09:08:51 +0700

    feat: add pin page

--- components/AuthPage.js
+++ components/AuthPage.js
@@ -9,7 +9,7 @@ import ButtonComp from "../components/ButtonComp";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
-const AuthPage = ({form}) => {
+const AuthPage = ({form, info}) => {
   return (
     <section>
       <Row>
@@ -28,21 +28,11 @@ const AuthPage = ({form}) => {
         </Col>
         <Col sm={12} lg={6} className={styles.right}>
          <div className="p-5">
-          <h2>Start Accessing Banking Needs <br />With All Devices and All Platforms<br/> With 30.000+ Users</h2>
-          <p className="my-5">Transfering money is eassier than ever, you can access Zwallet wherever you are. Desktop, laptop, mobile phone? we cover all of that for you!</p>
-          {/* <form>
-           <InputAuth IconElement={<AiOutlineMail className={`${styles.icon} fs-4 position-absolute`}/>} type='text' placehld='e-mail' />
-           <InputAuth IconElement={<VscLock className={`${styles.icon} fs-4 position-absolute`}/>} type='password' placehld='password'  />
-          </form>
-          <Link href='/forgot'>
-            <a className='d-flex justify-content-end text-decoration-none my-5'>Forgot password?</a>
-          </Link>
-          <div className="mt-5">
-            <ButtonComp block cls='mt-5'>AuthPage</ButtonComp>
-          </div>
-          <Link href='/signup'>
-            <a className="text-decoration-none d-flex justify-content-end mt-4">Dont have an account? Lets <span className="text-primary fw-bold">Signup</span></a>
-          </Link> */}
+          {info 
+            ? info
+            : <><h2>Start Accessing Banking Needs <br />With All Devices and All Platforms<br/> With 30.000+ Users</h2>
+              <p className="my-5">Transfering money is eassier than ever, you can access Zwallet wherever you are. Desktop, laptop, mobile phone? we cover all of that for you!</p></>
+          }
           {form}
          </div>
         </Col>
--- components/ButtonComp.js
+++ components/ButtonComp.js
@@ -1,11 +1,11 @@
 import { useRouter } from "next/dist/client/router";
 
-const ButtonComp = ({block = false, variant='primary', cls, color, route, children, ...rest}) => {
+const ButtonComp = ({block = false, variant='primary', cls, color, event, route, children, ...rest}) => {
   const router = useRouter()
 
   return (
     <div className={block ? 'd-grid' : 'd-inline-block'}>
-        <button onClick={() => router.push(route)} className={`btn btn-${variant} ${cls} fw-bold py-3 px-5 text-${color}`} {...rest}>{children}</button>
+        <button onClick={event} className={`btn btn-${variant} ${cls} fw-bold py-3 px-5 text-${color}`} {...rest}>{children}</button>
     </div>
   )
 }
