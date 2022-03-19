4865d8587bb5791f350f7e2ced32f5fb7e6ac880
rintosaputro <saputrorinto@gmail.com>
2022-03-19 07:18:28 +0700

    feat: add right section

--- components/SideBar.js
+++ components/SideBar.js
@@ -22,24 +22,40 @@ const SideBar = () => {
   ]
 
   return (
-    <aside className='col-lg-4'>
-      <div className='card p-4 bg-light position-relative'>
+    <div className='card p-4 bg-light position-relative h-100'>
         {dataSide.map((data, index) => {
           return <Link href={data.to} key={data.desc} className=' d-flex flex-row align-items-center my-3'>
-            <a className={`${routes === data.to && 'active-side'} side-side text-decoration-none text-white my-4 ps-2`}>
+            <a className={`${routes === data.to ? 'active-side text-primary': 'text-white'} side-side text-decoration-none my-4 ps-2`}>
               <data.icon className='fs-5 me-2'/>
               <span>{data.desc}</span>
             </a>
           </Link>
         })}
-        <Link href='/login' className=' d-flex flex-row align-items-center my-3 position-absolute'>
-          <a className='text-decoration-none text-white my-4 ps-3'>
+        <Link href='/login' className=' d-flex flex-row align-items-center my-3'>
+          <a className='text-decoration-none text-white my-4 ps-3 position-absolute bottom-0'>
             <MdOutlineLogout className='fs-5 me-2'/>
             <span>Logout</span>
           </a>
         </Link>
       </div>
-    </aside>
+    //   <aside className='col-lg-4'>
+    //   <div className='card p-4 bg-light position-relative'>
+    //     {dataSide.map((data, index) => {
+    //       return <Link href={data.to} key={data.desc} className=' d-flex flex-row align-items-center my-3'>
+    //         <a className={`${routes === data.to && 'active-side'} side-side text-decoration-none text-white my-4 ps-2`}>
+    //           <data.icon className='fs-5 me-2'/>
+    //           <span>{data.desc}</span>
+    //         </a>
+    //       </Link>
+    //     })}
+    //     <Link href='/login' className=' d-flex flex-row align-items-center my-3 position-absolute'>
+    //       <a className='text-decoration-none text-white my-4 ps-3'>
+    //         <MdOutlineLogout className='fs-5 me-2'/>
+    //         <span>Logout</span>
+    //       </a>
+    //     </Link>
+    //   </div>
+    // </aside>
   )
 }
 
--- pages/home.js
+++ pages/home.js
@@ -1,32 +1,86 @@
 import Layout from '../components/Layout';
 import styles from '../styles/Home.module.css';
-import { Row } from 'react-bootstrap';
+import Link from 'next/link';
+import { Row, Col } from 'react-bootstrap';
 import SideBar from '../components/SideBar';
+import { BsArrowUp } from 'react-icons/bs';
+import ChartCard from '../components/ChartCard';
 
 const Home = () => {
+
+  const transaction = [
+    {image: '/img/review-2.jpg', name: 'Samuel Sushi', status: 'Accept', total: 50000},
+    {image: '/img/review-2.jpg', name: 'Samuel Sushi', status: 'Transfer', total: 50000},
+    {image: '/img/review-2.jpg', name: 'Samuel Sushi', status: 'Transfer', total: 50000},
+    {image: '/img/review-2.jpg', name: 'Samuel Sushi', status: 'Accept', total: 50000}
+  ]
+
   return (
     <Layout>
       <main className={`${styles.contain} container`}>
         <Row className=''>
-          <SideBar />
-          {/* <aside className='col-lg-4'>
-            <div className='card p-4 bg-light position-relative'>
-              {dataSide.map((data, index) => {
-                return <Link href={data.to} key={data.desc} className=' d-flex flex-row align-items-center my-3'>
-                  <a className={`${routes === data.to && 'active-side'} side-side text-decoration-none text-white my-4 ps-2`}>
-                    <data.icon className='fs-5 me-2'/>
-                    <span>{data.desc}</span>
-                  </a>
-                </Link>
-              })}
-              <Link href='/login' className=' d-flex flex-row align-items-center my-3 position-absolute'>
-                <a className='text-decoration-none text-white my-4 ps-3'>
-                  <MdOutlineLogout className='fs-5 me-2'/>
-                  <span>Logout</span>
-                </a>
-              </Link>
+          <aside className='col-lg-4'>
+            <SideBar />
+          </aside>
+          <section className='col-lg-8'>
+            <div className='card p-4 bg-primary'>
+              <Row className='text-dark'>
+                <Col xs={12} md={6} className='d-flex justify-content-between flex-column'>
+                  <div>Balance</div>
+                  <h2>Rp 120.000</h2>
+                  <div>+623434215</div>
+                </Col>
+                <Col xs={12} md={6} className='d-flex justify-content-end'>
+                  <div className='d-flex flex-column'>
+                    <Link href='/transfer' className=' d-flex flex-row align-items-center my-2'>
+                      <a className='text-decoration-none text-white my-2 py-3 px-5 bg-light btn'>
+                        <BsArrowUp className='fs-5 me-2'/>
+                        <span>Transfer</span>
+                      </a>
+                    </Link>
+                    <Link href='/top-up' className=' d-flex flex-row align-items-center my-2'>
+                      <a className='text-decoration-none text-white my-2 py-3 px-5 bg-light btn'>
+                        <BsArrowUp className='fs-5 me-2'/>
+                        <span>Top Up</span>
+                      </a>
+                    </Link>
+                  </div>
+                </Col>
+              </Row>
             </div>
-          </aside> */}
+            <Row>
+              <Col xs={12} lg={6} className='mt-3'>
+                <div className='card bg-light py-3 h-100'>
+                  <ChartCard dataChart={[10, 50, 200, 300, 250, 139, 90]} labels={['Sat','Sun','Mon','Tue', 'Wed', 'Thu', 'Fri']} income={2200000} expense={1500000} />
+                </div>
+              </Col>
+              <Col xs={12} lg={6} className='mt-3'>
+                <div className='card bg-light p-3 h-100'>
+                  <h4>Transaction History</h4>
+                  {transaction.map((data, index) => {
+                    return <div key={index} className='d-flex flex-row align-items-center justify-content-between'>
+                      <style jsx>
+                        {`.photo-trans {
+                            background-image: url(${data.image});
+                          }`
+                        }
+                      </style>
+                      <div className='d-flex flex-row my-2'>
+                        <div className={`${styles.phototransaction} photo-trans`}></div>
+                        <div className='ps-2'>
+                          <div>{data.name}</div>
+                          <div>{data.status}</div>
+                        </div>
+                      </div>
+                      <div className={`${data.status === 'Transfer' ? 'text-danger fw-bold' : 'text-primary fw-bold'}`}>
+                        {data.status === 'Transfer' ? '-' : '+'}Rp{data.total}
+                      </div>
+                    </div>
+                  })}
+                </div>
+              </Col>
+            </Row>
+          </section>
         </Row>
       </main>
     </Layout>
